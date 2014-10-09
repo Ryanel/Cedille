2db12830f7ae503ed76e1757e31fe6dbdb5b36f9
Corwin McKnight <lioneltabre@gmail.com>
2014-10-09 12:06:02 -0700

    Bootable SPARC port

---
+++ kernel/arch/sparc/sparc32/board.c
@@ -0,0 +1,7 @@
+#include <stdint.h>
+void log_low(char * s) {
+
+}
+void text_console_printchar(char c, uint8_t x, uint8_t y) {
+    //use openfirmware to print a char
+}
---
+++ kernel/arch/sparc/sparc32/init/start.s
@@ -0,0 +1,7 @@
+.section .text
+.align 16
+.global _start
+ 
+_start:
+    call _start                      ! loop forever
+          nop
