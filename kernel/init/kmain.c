4868db0afaf583dc008e842b35c1701df732333b
Corwin McKnight <lioneltabre@gmail.com>
2015-02-27 00:35:30 -0800

    Remove test scrolling code from idle_process

--- kernel/init/kmain.c
+++ kernel/init/kmain.c
@@ -9,16 +9,12 @@ void timing_system_engine_reportstatustoconsole();
 void kernel_doperiodic(int force, int tick);
 
 int kernel_idle_process() {
-	int i = 0;
 	for(;;) {
 		// Attempt to reschedule other processes unless things need to be done
 		// So, check if theres anything that needs to be done (like kernel state verification)
 		// Execute that, make sure everything is preemptable
 		// Then, attempt to reschedule processes if there isn't anything.
 		// Otherwise cpu time is wasted.
-		if(i != 500) {
-			printf("test%d\n",i++);
-		}
 	}
 	return 0;
 }
